95970a329e97df4ebb6a55c7f9ecf906a4e168f1
MateusRangel <mateus.bs.rangel@gmail.com>
2020-11-26 21:58:39 -0300

    Add getAll ranking service

---
+++ backend/application/Services/ranking.service.js
@@ -0,0 +1,36 @@
+const { Ranking } = require("../models/");
+
+const getAll = async () => {
+  try {
+    const ranking = await Ranking.findAll({
+      order: [
+        ["time", "ASC"],
+        ["attempts", "ASC"],
+      ],
+    });
+
+    return ranking;
+  } catch (error) {
+    console.log(error.message);
+    throw error;
+  }
+};
+
+const getById = async () => {};
+
+const create = async () => {
+  try {
+  } catch (error) {}
+};
+
+const edit = async () => {};
+
+const remove = async () => {};
+
+module.exports = {
+  getAll,
+  getById,
+  create,
+  edit,
+  remove,
+};
