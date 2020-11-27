626a1286d39a03932bb056015c27f819a1a16820
MateusRangel <mateus.bs.rangel@gmail.com>
2020-11-27 00:38:01 -0300

    Add create ranking controller

--- backend/application/Controllers/ranking.controller.js
+++ backend/application/Controllers/ranking.controller.js
@@ -1,5 +1,26 @@
 const RankingService = require("../Services/ranking.service");
 
+const create = async (req, res) => {
+  try {
+    if (!req.body.playerName)
+      res.status(400).json({ error: "O nome do jogador deve ser fornecido" });
+    if (!req.body.time)
+      res.status(400).json({ error: "O tempo deve ser fornecido" });
+    if (!req.body.attempts)
+      res
+        .status(400)
+        .json({ error: "O número de tentativas deve ser fornecido" });
+    if (!req.body.primeNumber)
+      res.status(400).json({ error: "O número primo deve ser fornecido" });
+
+    const newRanking = await RankingService.create(req.body);
+
+    return res.status(201).json(newRanking);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
+
 const getAll = async (req, res) => {
   try {
     const rankings = await RankingService.getAll();
@@ -22,4 +43,4 @@ const getById = async (req, res) => {
   }
 };
 
-module.exports = { getAll, getById };
+module.exports = { create, getAll, getById };
