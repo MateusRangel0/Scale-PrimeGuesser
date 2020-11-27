e5a6e9e4e4716f358c82bc4bed86d424066999bc
MateusRangel <mateus.bs.rangel@gmail.com>
2020-11-26 23:40:46 -0300

    Add ranking getById route

--- backend/application/Routes/ranking.routes.js
+++ backend/application/Routes/ranking.routes.js
@@ -4,5 +4,6 @@ const Controller = require("../Controllers/ranking.controller");
 const router = express.Router();
 
 router.get("/", Controller.getAll);
+router.get("/:id", Controller.getById);
 
 module.exports = router;
--- backend/routes.js
+++ backend/routes.js
@@ -3,10 +3,12 @@ const ranking = require("./application/Routes/ranking.routes");
 
 const routes = express.Router();
 
+// API is up?
 routes.get("/", (_, res) => {
   res.send("API CHECK :)");
 });
 
+// Ranking route
 routes.use("/ranking", ranking);
 
 module.exports = routes;
