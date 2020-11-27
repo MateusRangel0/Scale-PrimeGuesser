df1a4f8e523d4b1a5774c377b3c76a1a9eee6dcc
MateusRangel <mateus.bs.rangel@gmail.com>
2020-11-26 22:20:18 -0300

    Create route for gettAll rankings

--- backend/routes.js
+++ backend/routes.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const ranking = require("./application/Routes/ranking.routes");
 
 const routes = express.Router();
 
 routes.get("/", (_, res) => {
   res.send("API CHECK :)");
 });
+
+routes.use("/ranking", ranking);
 
 module.exports = routes;
