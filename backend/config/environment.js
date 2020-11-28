286cddef8323a9cbbe05e9314c417e2128fd9efd
MateusRangel <mateus.bs.rangel@gmail.com>
2020-11-28 19:34:48 -0300

    Change base_url config

--- backend/config/environment.js
+++ backend/config/environment.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const environment = {
   BASE_URL: process.env.BASE_URL,
-  PORT: process.env.PORT || 8080,
+  PORT: process.env.PORT || 3333,
 };
 
 module.exports = environment;
