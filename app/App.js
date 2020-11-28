4900f5219ff5e253b261fea36ccffc389f8ed30a
MateusRangel <mateus.bs.rangel@gmail.com>
2020-11-28 04:04:54 -0300

    Refactor and general config

--- app/App.js
+++ app/App.js
@@ -1,15 +1,9 @@
 import React from "react";
-import { Text, View } from "react-native";
 
-import { Container } from "./src/utils/generalStyles";
 import Routes from "./src/routes";
 
 // console.disableYellowBox = true;
 
 export default function App() {
-  return (
-    <Container>
-      <Text>I'm alive :D</Text>
-    </Container>
-  );
+  return <Routes />;
 }
--- app/src/routes.js
+++ app/src/routes.js
@@ -3,13 +3,19 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
+import Home from "./views/Home";
+
 const { Navigator, Screen } = createStackNavigator();
 
 export default function Routes() {
   return (
     <NavigationContainer>
       <Navigator>
-        <Screen name="HomePage" />
+        <Screen
+          name="Home"
+          component={Home}
+          options={{ title: "Prime Guesser!" }}
+        />
       </Navigator>
     </NavigationContainer>
   );
