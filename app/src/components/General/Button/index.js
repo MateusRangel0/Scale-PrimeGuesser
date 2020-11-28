45eb22deaff28a9bab8c88fbc0b2719b60cb68b6
MateusRangel <mateus.bs.rangel@gmail.com>
2020-11-28 04:06:18 -0300

    Create a general button component

---
+++ app/src/components/General/Button/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { ButtonContainer, NormalButton, Label } from "./style";
+
+export default function Button({ onClick, label }) {
+  return (
+    <ButtonContainer>
+      <NormalButton onPress={onClick && (() => onClick())}>
+        {label ? <Label>{label}</Label> : ""}
+      </NormalButton>
+    </ButtonContainer>
+  );
+}
---
+++ app/src/components/General/Button/style.js
@@ -0,0 +1,20 @@
+import styled from "styled-components/native";
+
+export const ButtonContainer = styled.View`
+  margin-top: 10px;
+  align-items: center;
+`;
+
+export const NormalButton = styled.TouchableOpacity`
+  margin-top: 10px;
+  background-color: #9b59b6;
+  border-radius: 3px;
+  align-items: center;
+  justify-content: center;
+  width: 100px;
+  height: 40px;
+`;
+
+export const Label = styled.Text`
+  font-size: 16px;
+`;
