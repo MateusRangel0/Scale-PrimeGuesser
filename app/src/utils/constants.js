d3554da9737a3a297bdfed33f2482a29e71e9511
MateusRangel <mateus.bs.rangel@gmail.com>
2020-11-28 23:05:10 -0300

    Add some utils

--- app/src/utils/constants.js
+++ app/src/utils/constants.js
@@ -1,2 +1,1 @@
-export const API_BASE_URL =
-  process.env.REACT_APP_API_URL || "http://localhost:3333";
+export const API_BASE_URL = "http://192.168.1.13:3333/api";
--- app/src/utils/generalStyles.js
+++ app/src/utils/generalStyles.js
@@ -1,8 +1,31 @@
 import styled from "styled-components/native";
 
-export const Container = styled.View`
+export const Container = styled.SafeAreaView`
   flex: 1;
   background-color: #f4ebf7;
   justify-content: center;
   align-items: center;
+`;
+
+export const InfoContainer = styled.View`
+  width: 80%;
+  align-items: center;
+`;
+
+export const TextContainer = styled.Text`
+  font-size: 18px;
+  text-align: center;
+`;
+
+export const Tittle = styled.Text`
+  font-weight: bold;
+  font-size: 26px;
+  text-align: center;
+  color: #8e44ad;
+`;
+
+export const Footer = styled.View`
+  position: absolute;
+  bottom: 10%;
+  left: ${(props) => (props.left ? "8%" : "65%")};
 `;
