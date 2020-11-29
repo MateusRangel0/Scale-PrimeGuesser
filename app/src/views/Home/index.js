4d8eede7f5d9a5919352bcdbb98dd978ade474ca
MateusRangel <mateus.bs.rangel@gmail.com>
2020-11-28 23:08:20 -0300

    Changes on Home screen

--- app/src/views/Home/index.js
+++ app/src/views/Home/index.js
@@ -1,15 +1,25 @@
 import React, { useEffect, useState } from "react";
 
-import { Container } from "../../utils/generalStyles";
-import { Tittle, InfoContainer, TextContainer } from "./style";
+import {
+  Container,
+  TextContainer,
+  InfoContainer,
+  Tittle,
+  Footer,
+} from "../../utils/generalStyles";
 import Button from "../../components/General/Button";
 import TextField from "../../components/General/TextField";
+import { View } from "react-native";
 
 export default function Home({ navigation }) {
   const [inputs, setInputs] = useState({ playerName: "" });
 
   function isValidInputs() {
-    return inputs.playerName && inputs.playerName.trim().length;
+    return inputs.playerName && inputs.playerName.trim().length > 0;
+  }
+
+  function handleRanking() {
+    navigation.navigate("Ranking");
   }
 
   function onSubmit() {
@@ -23,12 +33,33 @@ export default function Home({ navigation }) {
       <InfoContainer>
         <Tittle>Prime Guesser</Tittle>
         <TextContainer>
-          Pense em um número primo de 2 a 20000, e eu irei adivinha-lo. Quantas
+          Pense em um número primo de 1 a 20000, e eu irei adivinha-lo. Quantas
           tentativas irei precisar?
         </TextContainer>
-        <TextField setInputs={setInputs} placeholder="Digite o seu nome" />
-        <Button onClick={onSubmit} label={"Começar!"}></Button>
+        <TextField
+          placeholder="Digite o seu nome"
+          keyboardType={"default"}
+          inputName={"playerName"}
+          input={inputs}
+          setInput={setInputs}
+          width={"300px"}
+        />
+        <Button
+          onClick={onSubmit}
+          label={"Começar!"}
+          disabled={!isValidInputs()}
+        />
       </InfoContainer>
+      <Footer left={false}>
+        <TextContainer>
+          <Button onClick={handleRanking} label={"Ver Ranking"} />
+        </TextContainer>
+      </Footer>
+      <Footer left>
+        <TextContainer>
+          <Button label={"Números Primos"} />
+        </TextContainer>
+      </Footer>
     </Container>
   );
 }
--- app/src/views/Home/style.js
+++ app/src/views/Home/style.js
@@ -1,17 +1,1 @@
 import styled from "styled-components/native";
-
-export const InfoContainer = styled.View`
-  width: 80%;
-`;
-
-export const Tittle = styled.Text`
-  font-weight: bold;
-  font-size: 26px;
-  text-align: center;
-  color: #8e44ad;
-`;
-
-export const TextContainer = styled.Text`
-  font-size: 18px;
-  text-align: center;
-`;
