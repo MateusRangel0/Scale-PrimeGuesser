a0f6e6ff8f06b3ecb76f97cb46d33717a70f9716
MateusRangel <mateus.bs.rangel@gmail.com>
2020-11-28 23:07:50 -0300

    Add game route

--- app/src/routes.js
+++ app/src/routes.js
@@ -4,6 +4,8 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
 import Home from "./views/Home";
+import GameClues from "./views/GameClues";
+import Ranking from "./views/Ranking";
 
 const { Navigator, Screen } = createStackNavigator();
 
@@ -15,6 +17,16 @@ export default function Routes() {
           name="Home"
           component={Home}
           options={{ title: "Prime Guesser!" }}
+        />
+        <Screen
+          name="GameClues"
+          component={GameClues}
+          options={{ title: "Pistas" }}
+        />
+        <Screen
+          name="Ranking"
+          component={Ranking}
+          options={{ title: "Ranking" }}
         />
       </Navigator>
     </NavigationContainer>
