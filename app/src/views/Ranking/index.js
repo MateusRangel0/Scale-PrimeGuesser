0762d56b4f7179c7e7f82b0d91a8aad9625c6412
MateusRangel <mateus.bs.rangel@gmail.com>
2020-11-28 23:09:45 -0300

    Create ranking screen

---
+++ app/src/views/Ranking/index.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+import { View, Text } from "react-native";
+
+import api from "../../services/api";
+
+import {
+  Container,
+  InfoContainer,
+  Tittle,
+  TextContainer,
+} from "../../utils/generalStyles";
+import Button from "../../components/General/Button";
+
+export default function Ranking({ navigation }) {
+  useEffect(() => {
+    async function getRankings() {
+      const { data } = await api.get(`/ranking`);
+      console.log(data);
+    }
+    getRankings();
+  }, []);
+
+  return (
+    <View>
+      <Container>
+        <InfoContainer>
+          <Tittle>Ranking de Jogadores</Tittle>
+        </InfoContainer>
+      </Container>
+    </View>
+  );
+}
