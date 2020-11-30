b465181b317b7b4f34d5d3cf5204e6be385f7653
MateusRangel <mateus.bs.rangel@gmail.com>
2020-11-29 23:46:07 -0300

    Add config for api services

--- app/src/services/api.js
+++ app/src/services/api.js
@@ -1,9 +1,13 @@
+// libs
 import axios from "axios";
 
-import { API_BASE_URL } from "../utils/constants";
+// utils
+import { serverIP } from "../utils/constants";
 
-const api = axios.create({
-  baseURL: API_BASE_URL,
+const url = `http://${serverIP}:3333/api/`;
+
+const Api = axios.create({
+  baseURL: `${url}`,
 });
 
-export default api;
+export default Api;
--- app/src/utils/constants.js
+++ app/src/utils/constants.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = "http://192.168.1.13:3333/api";
+export const serverIP = `192.168.1.13`;
 
 export const NEXT_NUMBER_CONDITION = {
   DOWN: 0,
