6f24fbb8b860f6f1c265fff6d20135b3da45503b
MateusRangel <mateus.bs.rangel@gmail.com>
2020-11-30 03:13:57 -0300

    Changes on general button component

--- app/src/components/General/Button/index.js
+++ app/src/components/General/Button/index.js
@@ -2,11 +2,21 @@ import React from "react";
 
 import { ButtonContainer, NormalButton, Label } from "./style";
 
-export default function Button({ onClick, label, disabled }) {
+export default function Button({ onClick, label, disabled, isStart }) {
   return (
-    <ButtonContainer>
-      <NormalButton onPress={() => onClick()} disabled={disabled}>
-        {label ? <Label disabled={disabled}>{label}</Label> : ""}
+    <ButtonContainer isStart={isStart}>
+      <NormalButton
+        onPress={() => onClick()}
+        disabled={disabled}
+        isStart={isStart}
+      >
+        {label ? (
+          <Label disabled={disabled} isStart={isStart}>
+            {label}
+          </Label>
+        ) : (
+          ""
+        )}
       </NormalButton>
     </ButtonContainer>
   );
--- app/src/components/General/Button/style.js
+++ app/src/components/General/Button/style.js
@@ -1,7 +1,11 @@
 import styled from "styled-components/native";
 
 export const ButtonContainer = styled.View`
+  flex: ${(props) => (props.isStart ? "0.2" : "0.5")};
+  flex-direction: row;
   align-items: center;
+  justify-content: center;
+
   margin-top: 10px;
 `;
 
@@ -9,18 +13,19 @@ export const NormalButton = styled.TouchableOpacity`
   align-items: center;
   justify-content: center;
 
-  width: auto;
-  height: 50px;
+  width: ${(props) => (props.isStart ? "140px" : "100px")};
+  height: ${(props) => (props.isStart ? "80px" : "60px")};
 
+  border-radius: 10px;
   margin-top: 10px;
-  border-radius: 3px;
   padding-left: 10px;
   padding-right: 10px;
 
-  background-color: ${(props) => (props.disabled ? "#9e9e9e" : "#9b59b6")};
+  background-color: ${(props) => (props.disabled ? "#cccccc" : "#9b59b6")};
 `;
 
 export const Label = styled.Text`
-  font-size: ${(props) => (props.disabled ? "16px" : "17px")};
-  font-weight: ${(props) => (props.disabled ? "400" : "600")};
+  font-size: ${(props) => (props.isStart ? "18px" : "14px")};
+  font-weight: ${(props) => (props.disabled ? "400" : "bold")};
+  color: white;
 `;
