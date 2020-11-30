d5483743f26edbd14fcb5c6547cfd1dd3801e8dc
MateusRangel <mateus.bs.rangel@gmail.com>
2020-11-30 03:14:58 -0300

    Refactor on general style

--- app/src/utils/generalStyles.js
+++ app/src/utils/generalStyles.js
@@ -1,20 +1,25 @@
 import styled from "styled-components/native";
+import { StatusBar } from "react-native";
 
 export const Container = styled.SafeAreaView`
   flex: 1;
   background-color: #f4ebf7;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
 `;
 
 export const InfoContainer = styled.View`
+  flex: 1;
   width: 90%;
   align-items: center;
+  justify-content: center;
 `;
 
 export const TextContainer = styled.Text`
-  font-size: 18px;
+  font-size: ${(props) => (props.isNumber ? "28px" : "22px")};
   text-align: center;
+
+  font-weight: ${(props) => (props.isNumber ? "bold" : "400")};
 `;
 
 export const Tittle = styled.Text`
@@ -25,16 +30,21 @@ export const Tittle = styled.Text`
 `;
 
 export const Footer = styled.View`
-  position: absolute;
-  bottom: 10%;
-  left: ${(props) => (props.left ? "8%" : "65%")};
+  flex: 0.2;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+
+  width: 100%;
+  padding: 15px;
 `;
 
 export const Header = styled.View`
-  height: 64px;
+  height: 55px;
 
-  align-items: center;
   justify-content: center;
+  align-items: flex-end;
 
   margin-bottom: 15px;
+  margin-top: ${StatusBar.currentHeight}px;
 `;
